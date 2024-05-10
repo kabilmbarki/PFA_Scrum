10df2145131a29b1c6f4adb02844d4c57dc7642c
wassim-55 <wchahed55@gmail.com>
2024-05-10 07:39:03 +0100

    inscriPageee

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,333 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      home: Scaffold(
+        backgroundColor: const Color.fromARGB(255, 47, 57, 62),
+        body: ListView(
+          children: [
+            Container(
+              alignment: Alignment(0, 0),
+              width: 300,
+              height: 150,
+              child: Image.asset('assets/logo.png'),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            Container(
+              padding: const EdgeInsets.all(10),
+              child: Column(
+                children: [
+                  Container(
+                    width: 1000,
+                    height: 30,
+                    padding: const EdgeInsets.all(5),
+                    child: const Text(
+                      "Entrer votre nom",
+                      textAlign: TextAlign.start,
+                      style: TextStyle(
+                        fontStyle: FontStyle.italic,
+                        color: Colors.white, // Text color
+                        fontSize: 17, // Text font size
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.all(3),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30),
+                      border: Border.all(width: 2, color: Colors.white),
+                    ),
+                    padding: const EdgeInsets.all(7),
+                    width: 1000,
+                    height: 50,
+                    child: const TextField(
+                      decoration: InputDecoration(
+                        hintText: 'nom',
+                        hintStyle: TextStyle(color: Colors.grey),
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: 1000,
+                    height: 30,
+                    padding: const EdgeInsets.all(5),
+                    child: const Text(
+                      "Entrer votre prénom",
+                      textAlign: TextAlign.start,
+                      style: TextStyle(
+                        fontStyle: FontStyle.italic,
+                        color: Colors.white,
+                        fontSize: 17,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.all(3),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30),
+                      border: Border.all(width: 2, color: Colors.white),
+                    ),
+                    padding: const EdgeInsets.all(7),
+                    width: 1000,
+                    height: 50,
+                    child: const TextField(
+                      decoration: InputDecoration(
+                        hintText: 'prénom',
+                        hintStyle: TextStyle(color: Colors.grey),
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: 1000,
+                    height: 30,
+                    padding: const EdgeInsets.all(5),
+                    child: const Text(
+                      "Entrer le nom d'Entreprise",
+                      textAlign: TextAlign.start,
+                      style: TextStyle(
+                        fontStyle: FontStyle.italic,
+                        color: Colors.white,
+                        fontSize: 17,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.all(3),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30),
+                      border: Border.all(width: 2, color: Colors.white),
+                    ),
+                    padding: const EdgeInsets.all(7),
+                    width: 1000,
+                    height: 50,
+                    child: const TextField(
+                      decoration: InputDecoration(
+                        hintText: "nom d'entreprise",
+                        hintStyle: TextStyle(color: Colors.grey),
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                  // Additional text fields and containers
+                  Container(
+                    width: 1000,
+                    height: 30,
+                    padding: const EdgeInsets.all(5),
+                    child: const Text(
+                      "Entrer votre adresse Mail",
+                      textAlign: TextAlign.start,
+                      style: TextStyle(
+                        fontStyle: FontStyle.italic,
+                        color: Colors.white,
+                        fontSize: 17,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.all(3),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30),
+                      border: Border.all(width: 2, color: Colors.white),
+                    ),
+                    padding: const EdgeInsets.all(7),
+                    width: 1000,
+                    height: 50,
+                    child: const TextField(
+                      decoration: InputDecoration(
+                        hintText: 'adresse Mail',
+                        hintStyle: TextStyle(color: Colors.grey),
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                  // More text fields and containers can be added similarly
+                  Container(
+                    width: 1000,
+                    height: 30,
+                    padding: const EdgeInsets.all(5),
+                    child: const Text(
+                      "Créer un mot de passe",
+                      textAlign: TextAlign.start,
+                      style: TextStyle(
+                        fontStyle: FontStyle.italic,
+                        color: Colors.white,
+                        fontSize: 17,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.all(3),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30),
+                      border: Border.all(width: 2, color: Colors.white),
+                    ),
+                    padding: const EdgeInsets.all(7),
+                    width: 1000,
+                    height: 50,
+                    child: const TextField(
+                      decoration: InputDecoration(
+                        hintText: 'mot de passe',
+                        hintStyle: TextStyle(color: Colors.grey),
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: 1000,
+                    height: 30,
+                    padding: const EdgeInsets.all(5),
+                    child: const Text(
+                      "Confirmer le mot de passe",
+                      textAlign: TextAlign.start,
+                      style: TextStyle(
+                        fontStyle: FontStyle.italic,
+                        color: Colors.white,
+                        fontSize: 17,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.all(3),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30),
+                      border: Border.all(width: 2, color: Colors.white),
+                    ),
+                    padding: const EdgeInsets.all(7),
+                    width: 1000,
+                    height: 50,
+                    child: const TextField(
+                      decoration: InputDecoration(
+                        hintText: 'confirmer mot de passe',
+                        hintStyle: TextStyle(color: Colors.grey),
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: 1000,
+                    height: 30,
+                    padding: const EdgeInsets.all(5),
+                    child: const Text(
+                      "Entrer votre numéro de téléphone",
+                      textAlign: TextAlign.start,
+                      style: TextStyle(
+                        fontStyle: FontStyle.italic,
+                        color: Colors.white,
+                        fontSize: 17,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.all(3),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30),
+                      border: Border.all(width: 2, color: Colors.white),
+                    ),
+                    padding: const EdgeInsets.all(7),
+                    width: 1000,
+                    height: 50,
+                    child: const TextField(
+                      decoration: InputDecoration(
+                        hintText: 'numéro de téléphone',
+                        hintStyle: TextStyle(color: Colors.grey),
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(top: 30),
+                    width: 200,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      border: Border.all(
+                        color: Color.fromARGB(255, 158, 96, 2),
+                        width: 1,
+                      ),
+                    ),
+                    child: TextButton(
+                      onPressed: () {
+                        print("S'inscrire button pressed!");
+                      },
+                      child: const Text(
+                        "S'inscrire",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontWeight: FontWeight.w700,
+                          fontSize: 22,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: 500,
+                    margin: const EdgeInsets.all(10),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text(
+                          "si vous n’avez pas un compte : ",
+                          textAlign: TextAlign.start,
+                          style: TextStyle(
+                            fontSize: 13,
+                            color: Colors.white,
+                          ),
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.of(context).push(MaterialPageRoute(
+                              builder: (context) => const SignIn(),
+                            ));
+                          },
+                          child: const Text(
+                            "inscrirez vous",
+                            style: TextStyle(
+                              fontSize: 15,
+                              color: Color.fromRGBO(181, 64, 9, 1),
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+class SignIn extends StatelessWidget {
+  const SignIn({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sign In Page'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text('Go Back'),
+        ),
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(MyApp());
 }
